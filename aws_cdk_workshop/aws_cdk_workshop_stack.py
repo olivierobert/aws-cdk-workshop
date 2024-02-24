cc499834cf188e8b880fea599f28308fe4961500
Olivier Robert <olivierobert@gmail.com>
2024-02-24 14:34:27 +0100

    Add API gateway

--- aws_cdk_workshop/aws_cdk_workshop_stack.py
+++ aws_cdk_workshop/aws_cdk_workshop_stack.py
@@ -2,6 +2,7 @@ from constructs import Construct
 from aws_cdk import (
     Stack,
     aws_lambda as _lambda,
+    aws_apigateway as apigw
 )
 
 
@@ -11,9 +12,14 @@ class AwsCdkWorkshopStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # Defines an AWS Lambda resource
-        hello_lambda = _lambda.Function(
+        hello_service = _lambda.Function(
             self, 'HelloHandler',
             runtime=_lambda.Runtime.PYTHON_3_12,
             code=_lambda.Code.from_asset('aws_cdk_workshop/lambda'),
             handler='hello.handler'
+        )
+
+        apigw.LambdaRestApi(
+            self, 'Endpoint',
+            handler=hello_service
         )
